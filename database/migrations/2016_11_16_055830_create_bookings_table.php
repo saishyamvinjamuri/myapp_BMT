afad3961dc0dd10df6d2596aa09ab287e08168b3
SaiShyamVinjamuri <vinjamurisaishyam@gmail.com>
2016-11-16 00:06:03 -0600

    authentication successful login and logout

---
+++ database/migrations/2016_11_16_055830_create_bookings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {   
+        //table which is used to save slots of users
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('email')->unique();
+            $table->time('bookedfrom');
+            $table->time('bookedtill');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
